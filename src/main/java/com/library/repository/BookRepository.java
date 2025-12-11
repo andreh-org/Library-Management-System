83815f53c8f4844afee415ee603256bd04e79eea
andrehkhouri333 <andrehkhouri333@gmail.com>
2025-12-11 21:04:02 +0200

    Update BookRepository.java

--- src/main/java/com/library/repository/BookRepository.java
+++ src/main/java/com/library/repository/BookRepository.java
@@ -3,7 +3,6 @@ package com.library.repository;
 import com.library.model.Book;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 /**
  * Repository for managing book data
